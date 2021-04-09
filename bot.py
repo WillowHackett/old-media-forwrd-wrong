3a0085874bee98f90e81cd812f9329899c71a847
insane-man220 <76524189+insane-man220@users.noreply.github.com>
2021-04-09 21:57:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,16 @@
-import logging
-import logging.config
+from pyrogram import Client, filters
 
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.ERROR)
+API_ID = 'YOUR API ID'
+API_HASH = 'YOUR API HASH'
+USERBOT_STRING_SESSION = 'USERBOT STING SESSION'
+CHANNEL = 'PRIVATE CHANNEL USERNAME OR ID'
 
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
-from utils import Media
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN
+bot = Client(STRING_SESSION, API_ID, API_HASH)
 
 
-class Bot(Client):
-
-    def __init__(self):
-        super().__init__(
-            session_name=SESSION,
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
-        )
-
-    async def start(self):
-        await super().start()
-        await Media.ensure_indexes()
-        me = await self.get_me()
-        self.username = '@' + me.username
-        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-
-    async def stop(self, *args):
-        await super().stop()
-        print("Bot stopped. Bye.")
+@bot.on_message(filters.incoming & filters.channel & ~filters.chat(CHANNEL))
+async def forward(bot, message):
+    await message.forward(CHANNEL)
 
 
-app = Bot()
-app.run()
+bot.run()
